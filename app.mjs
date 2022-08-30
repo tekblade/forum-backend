c06c3d461947bef08c2de72a90df8702a744141f
Łukasz Grabias <teknomanlance@wp.pl>
2022-08-30 22:16:08 +0200

    NodeJs backend forum

--- app.mjs
+++ app.mjs
@@ -3,47 +3,47 @@ import * as fs from 'fs';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import bodyParser from "body-parser";
-import AsyncLock from "async-lock";
-
+import cors from "cors";
 var app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-var key="";
+
 
 
 
 app.post("/register",function(req,res){
-      res.setHeader('Content-Type', 'application/json');
+
       fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
       var users=JSON.parse(data);
       users["users"].push(req.body);
       var check=false;
       fs.writeFile(__dirname + "/" + "users.json", JSON.stringify(users),'utf8', function (err) {
          if(err){
-            res.send(err)
+            res.send("error");
             check=true;
          }
       })
       if(!check){
-         res.send("Created"); 
+         res.send(JSON.stringify("Created")); 
       }
            
    })
 })
-app.post("/login",function(req,res){   
+app.post("/login",function(req,res){
    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
       var users=JSON.parse(data);
       var check=false;
       for(let i=0;i<users["users"].length;i++){
          if(users["users"][i]["name"]==req.body["name"] && users["users"][i]["password"]==req.body["password"]){
-            res.send("logged");
+            res.send(JSON.stringify("logged"));
             check=true;
          }
       }
       if(!check)
-      res.send("Acess Denied");
+      res.send(JSON.stringify("Acess Denied"));
       })
    
            
